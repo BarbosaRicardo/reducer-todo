fd2580280e1e91a9ea50d9d91cc0fd289fc8850f
Ricardo <ricardo@Ricardos-Mac-mini.local>
2019-12-10 14:06:14 -0800

    completed dispatch

--- src/src/App.js
+++ src/src/App.js
@@ -7,20 +7,26 @@
 
 import React, { useReducer } from 'react';
 
-import { initialState, reducer } from './reducers/reducer'
-import TodoForm from './components/TodoForm.js';
+import {initialState, reducer} from './reducers/reducer'
+import TodoForm from './components/TodoForm.js'
+import Todo from './components/Todo.js'
+
 
 import './App.css';
 
 function App() {
+
   const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-        <TodoForm dispatch={dispatch} />
-        <button onClick{() => dispatch({type: "removeCompleted" })}>Remove Complete</button>
-
+    <TodoForm dispatch ={dispatch}/>
+    <button onClick={() => dispatch({type: "removeCompleted"})}>Remove Complete</button>
+      {state.map(item => {
+        return <Todo key={item.id} item={item} dispatch = {dispatch}/>
+      })}
     </div>
   );
 }
 
 export default App;
+
